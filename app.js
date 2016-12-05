fcda4fc4c4c026f7c60c8d33850ab6e4d6695f01
Christina Bergstrom <bergstromcbb@gmail.com>
2016-12-04 23:10:09 -0500

    inital commit of custom recipe page and major update to javascript as well as starting of css page

--- app.js
+++ app.js
@@ -3,19 +3,58 @@ var app = angular.module("recipeCook", ["ngRoute"]); // create the module
 
 app.config(function($routeProvider) {
     $routeProvider.when("/popular", {
-        templateUrl: "views/popular.html"
+        templateUrl: "views/popular.html",
+        controller:""
     });
     $routeProvider.when("/custom", {
         templateUrl: "views/custom.html",
-        controller: ""
+        controller: "enterFood"
     });
     $routeProvider.when("/pairings", {
         templateUrl: "views/pairings.html",
-        controller: ""
+        controller: "displayFood"
     });
     $routeProvider.otherwise({
         template: "This is the default."
     });
 });
 
+app.factory("recipeStore", function(){
+    var recipe={};
+    return{
+        setRecipe: function(food){
+            recipe = food;
+        },
+        getRecipe: function(){
+            return recipe;
+        }
+    };
+
+});
+
+app.controller('enterFood', function($scope, recipeStore){
+
+  $scope.onclick = function(foodsToAdd){
+
+    var recipe = $scope.recipeData;
+
+    var foodsToMatch = [recipe.protein, recipe.vegetable, recipe.starch];
+
+    recipeStore.setRecipe(recipe);
+
+    console.log(foodsToMatch);
+    location.hash = "/pairings";
+};
+
+});
+
+app.controller('displayFood', function($scope, recipeStore){
+  var recipe = recipeStore.getRecipe();
+
+  $scope.recipe = recipe;
+});
+
 })();
+
+
+
