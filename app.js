7f6805dd20944236aa4f61fc069bb9cdb29ad91a
Christina Bergstrom <bergstromcbb@gmail.com>
2016-12-06 11:14:20 -0500

    updating files

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.controller('displayRecipes', function($scope, $http){
 
 $http({
   method: 'GET',
-  url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=' + '5',
+  url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=5',
   headers: {
    'X-Mashape-Key': "Qajqo1J4xdmshNRgkEbboXTYJFJYp19ne8jjsnq96e872bitro"
     }
