adf1f34f99300e70787433017809e964f974106b
Cameron Shurney <cshurney45@gmail.com>
2016-12-06 09:51:03 -0500

    no change

--- app.js
+++ app.js
@@ -59,10 +59,11 @@ app.controller('displayFood', function($scope, recipeStore){
 
 
 
-//API key FoodToFork---> 873e64556154738153c31e102ea6836f 
+
+//API key FoodToFork---> 873e64556154738153c31e102ea6836f
 
 // var xhr = new XMLHttpRequest();
- 
+
 // xhr.onload = function() {
 //   if (xhr.status === 200) {
 //     console.log(xhr.status);
@@ -94,7 +95,7 @@ request.setRequestHeader("X-Mashape-Key", "Qajqo1J4xdmshNRgkEbboXTYJFJYp19ne8jjs
 
 // var Request = unirest.get('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/jokes/random');
 
-// //http://food2fork.com/api/search?key=873e64556154738153c31e102ea6836f 
+// //http://food2fork.com/api/search?key=873e64556154738153c31e102ea6836f
 // unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/jokes/random")
 // .header("X-Mashape-Key", "Qajqo1J4xdmshNRgkEbboXTYJFJYp19ne8jjsnq96e872bitro")
 // .header("Accept", "application/json")
@@ -104,8 +105,3 @@ request.setRequestHeader("X-Mashape-Key", "Qajqo1J4xdmshNRgkEbboXTYJFJYp19ne8jjs
 // //http://food2fork.com/api/get?key=873e64556154738153c31e102ea6836f
 // //http://www.recipepuppy.com/api/
 request.send(null);
-
-
-
-
-
