77fe11466599d644b928d042b72944ca2fd2c8b9
Simona Golebiewski <colfescus@yahoo.com>
2016-12-07 09:52:12 -0500

    stashing css

--- app.js
+++ app.js
@@ -86,11 +86,7 @@ $http({
   // headers: {
   //  'X-Mashape-Key': "Qajqo1J4xdmshNRgkEbboXTYJFJYp19ne8jjsnq96e872bitro"
   //   }
-  url: 
-  'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=5',
-  headers: {
-   'X-Mashape-Key': "Qajqo1J4xdmshNRgkEbboXTYJFJYp19ne8jjsnq96e872bitro"
-    }
+ 
 
 }).then(function successCallback(response) {
             document.body.className = 'ok';
